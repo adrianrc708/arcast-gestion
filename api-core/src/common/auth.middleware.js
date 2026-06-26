8f6dc7f02061ed40fc3e5c8c9cdb269ca4f90a3b
Marcelo <marcelo.palacios@unmsm.edu.pe>
2026-06-26 18:09:10 -0500

    Correción de bug en 'auth.middleware.js'

--- api-core/src/common/auth.middleware.js
+++ api-core/src/common/auth.middleware.js
@@ -1,41 +1,44 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 exports.optionalAuth = (req, res, next) => {
-    const token = req.header('x-auth-token') || req.header('Authorization');
-    if (!token) return next();
+  const token = req.header("x-auth-token") || req.header("Authorization");
+  if (!token) return next();
 
-    try {
-        const tokenString = token.startsWith('Bearer ') ? token.slice(7) : token;
-        req.user = jwt.verify(tokenString, process.env.JWT_SECRET);
-        next();
-    } catch (err) { 
-        next(); // Si el token falla, lo dejamos pasar como anónimo
-    }
+  try {
+    const tokenString = token.startsWith("Bearer ") ? token.slice(7) : token;
+    req.user = jwt.verify(tokenString, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    next(); // Si el token falla, lo dejamos pasar como anónimo
+  }
 };
 
 exports.requiredAuth = (req, res, next) => {
-    const token = req.header('x-auth-token') || req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'No hay token.' });
+  const token = req.header("x-auth-token") || req.header("Authorization");
+  if (!token) return res.status(401).json({ message: "No hay token." });
 
-    try {
-        const tokenString = token.startsWith('Bearer ') ? token.slice(7) : token;
-        req.user = jwt.verify(tokenString, process.env.JWT_SECRET);
-        next();
-    } catch (err) { res.status(401).json({ message: 'Token no válido.' }); }
+  try {
+    const tokenString = token.startsWith("Bearer ") ? token.slice(7) : token;
+    req.user = jwt.verify(tokenString, process.env.JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token no válido." });
+  }
 };
 const authorize = (roles = []) => {
-    return (req, res, next) => {
-        if (!req.user) {
-            return res.status(401).json({ message: 'No autenticado.' });
-        }
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "No autenticado." });
+    }
 
-        if (!roles.includes(req.user.role)) {
-            return res.status(403).json({ message: 'No tienes permisos para realizar esta acción.' });
-        }
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({ message: "No tienes permisos para realizar esta acción." });
+    }
 
-        next();
-    };
+    next();
+  };
 };
 
-module.exports = { optionalAuth, requiredAuth };
+exports.authorize = authorize;
